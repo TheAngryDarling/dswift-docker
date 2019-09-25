432a12f04755a6b06795209989a993b6b988696e
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-09-24 22:24:30 -0400

    Copied out some of the logic from the updatder script into the dockerfile to allow for better use of the caching

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 __dswift_docker_publish() {
     if [ "$1" == "1" ]; then 
-        echo "Publishing image $dockertag"; 
+        echo "Publishing image $2"; 
         docker push $2
     fi
 }
@@ -20,6 +20,7 @@ __dswift_docker_file_build() {
     local dockerUser="theangrydarling"
     local templateFile="Dockerfile.template"
     local dockerFile="Dockerfile"
+    local shaFile="dswift.sha"
     if [ ! -f $templateFile ]; then
         echo "File $templateFile not found!"
         return 1
@@ -27,6 +28,10 @@ __dswift_docker_file_build() {
 
     local publishImage=0
 
+    local DSWIFT_REPOSITORY="https://github.com/TheAngryDarling/dswift"
+    local DSWIFT_BRANCH_LATEST="latest"
+    local DSWIFT_REF_TAG="refs/tags/$DSWIFT_BRANCH_LATEST"
+    local DSWIFT_SED_REF_TAG="$(echo $DSWIFT_REF_TAG | sed 's/\//\\\//g')"
     local swiftTags=( $(curl -L -s 'https://registry.hub.docker.com/v2/repositories/library/swift/tags?page_size=1024' | grep -o -E '"name": "[A-Za-z0-9\.\-]+",' | sed 's/"name": "//g' | sed 's/",//g') )
     local swiftVersions=( )
     local tempArray=()
@@ -55,27 +60,48 @@ __dswift_docker_file_build() {
     fi
 
     unset tempArray
+
+    currentLocation=$(pwd)
+    templateFilePath="$currentLocation/$templateFile"
+    tmp_docker_root=$(mktemp -d -t dswift-)
+    cd "$tmp_docker_root"
+
+    echo "Downloading Latest Source Code"
+    curl -L https://github.com/TheAngryDarling/dswift/archive/latest.tar.gz --output dswift.tar.gz 2>/dev/null
+    tar xzf dswift.tar.gz
+    #cp -r ~/development/swift/Executables/dswift ./dswift-latest
+
+    echo "Downloading Latest Source Code SHA"
+    latestSHA=$(git ls-remote --refs $DSWIFT_REPOSITORY $DSWIFT_REF_TAG | sed -e "s/$DSWIFT_SED_REF_TAG//g" -e 's/[[:space:]]//g')
+    echo $latestSHA > $shaFile
+
+    echo "Downloading Updater Script"
+    curl https://raw.githubusercontent.com/TheAngryDarling/dswift/master/dswift-update --output dswift-update 2>/dev/null
+    
+
     for i in "${swiftVersions[@]}" ; do
         
         dockertag="$dockerUser/dswift:$i"
-        if [[ "$(docker images -q $dockertag 2> /dev/null)" != "" ]]; then
-            # do something
-            echo "Removing existing image for $dockertag"
-            docker images rm -f $dockertag
-        fi
         echo "Creating docker file for $dockertag" \
-        && mkdir $i \
-        && cp -n "$templateFile" "$i/$dockerFile" \
+        && mkdir "$i" \
+        && cp -n "$templateFilePath" "$i/$dockerFile" \
+        && cp -r "dswift-latest" "$i/dswift-latest" \
+        && cp "$shaFile" "$i/$shaFile" \
+        && cp "dswift-update" "$i/dswift-update" \
         && cd "$i" \
         && sed -i.bak "s/\$SWIFT_TAG/$i/g" "$dockerFile" && rm "$dockerFile".bak \
         && echo "Building image $dockertag" \
         && docker build -t $dockertag . \
         && __dswift_docker_publish "$publishImage" "$dockertag" \
-        && cd .. \
-        && rm -r -f $i 
+        && cd ".." \
+        && rm -r -f "$i" 
         
        
     done
+
+    cd "$currentLocation"
+    echo "Cleaning up..."
+    rm -r -f "$tmp_docker_root"
 }
 
 __dswift_docker_file_build $@
